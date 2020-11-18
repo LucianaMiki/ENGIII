2f2b1c84ff505e5a01f56b40d8ef99a45b83861c
LucianaMiki <57202211+LucianaMiki@users.noreply.github.com>
2020-11-18 16:05:22 -0300

    MVC 0.2
    Adicionado pastas Command, DAO, Strategy e ViewHelper
    Lendo apenas o código da sala.

--- assets/js/http.js
+++ assets/js/http.js
@@ -3,5 +3,15 @@ function getValues(){
     var e = document.getElementById("codigo");
     var codigo = e.options[e.selectedIndex].text;
     console.log(codigo);
+    
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            alert("Sucesso: " + codigo);
+        }
+    };
+    xhttp.open("GET", "xmlhttp_info.txt", true);
+    xhttp.send();
 
 }
+
